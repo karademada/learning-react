7bdd28e0952339787eefc3ccf9c455607554edad
Christophe Lazantsy <christophe.lazantsy@gmail.com>
2018-06-03 13:32:16 +0200

    add routes and navigation component

--- src/routes.js
+++ src/routes.js
@@ -1,4 +1,3 @@
-import React from 'react';
 import {
   BrowserRouter as Router,
   Route
@@ -7,12 +6,14 @@ import About from './components/About';
 import Sample from './components/Sample';
 import Menu from './components/Menu';
 import data from '../data/recipes.json';
+import Navigation from './components/Navigation';
 
 console.log(data);
 
 const Routes = () => (
   <Router>
     <div>
+    <Navigation />
       <Route exact path="/" render={() =>(
         <Menu recipes={data} />
       )}/>
